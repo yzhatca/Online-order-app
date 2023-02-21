b90c8a3529869867d1588daaae985b6352f7dd54
unknown <yzharsenal@gmail.com>
2023-02-20 22:55:53 -0500

    first version of online food order app with dummy data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from "./App";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// 设置移动端的适配
+// 除以几视口的宽度就是多少rem，现在我们设置视口的总宽度为750rem
+document.documentElement.style.fontSize = 100 / 750 + 'vw';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
